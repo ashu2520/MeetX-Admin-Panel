cd531d062dc2af6cf8dd9dca8421ce0da5d56c3e
Pratyush <pratyushraghuvanshi73@gmail.com>
2024-08-14 17:40:09 +0530

    Updated the ui of managetemplate and edit

--- client/src/pages/EditTemplate/EditTemplate.jsx
+++ client/src/pages/EditTemplate/EditTemplate.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import Header from "../../components/Header";
+import NavBar from "../../components/NavBar";
 import Sidebar from "../../components/Sidebar";
 import Editor from "../../components/EditTemplate/Editor";
 
 const EditTemplate = () => {
   return (
     <>
-      <Header />
-      <div className="flex justify-between px-12">
+      <NavBar />
+      <div className="flex">
         <Sidebar />
-        <div className="w-full">
-          <div className="p-2.5 rounded-md bg-white m-3 shadow-sm text-black sm:text-base text-xs self-stretch border">
-            <h1 className="bg-[#214139] text-white text-left py-1.5 pl-5 text-base">
+        <div className="w-full p-4">
+          <div className="rounded-md bg-white shadow-sm text-black sm:text-base text-xs border">
+            <h1 className="bg-[#DBE3FF]  text-balck text-left py-2 pl-5 text-base">
               EDIT EMAIL TEMPLATE
             </h1>
             <div className="m-3 flex flex-col border p-2">
-                <div className="flex gap-3">
+              <div className="flex gap-3">
                 <label className="text-xl">Subject :</label>
-                <input type="text" className="border border-black rounded-xl w-[80%] text-base p-2"/>
-                </div>
-                <div className="text-xl my-5">Body : </div>
-              <Editor/>
+                <input
+                  type="text"
+                  className="border border-black rounded-xl w-[80%] text-base p-2"
+                />
+              </div>
+              <div className="text-xl my-5">Body : </div>
+              <Editor />
             </div>
           </div>
         </div>
--- client/src/pages/ManageTemplates/ManageTemplates.jsx
+++ client/src/pages/ManageTemplates/ManageTemplates.jsx
@@ -1,47 +1,40 @@
 import React from "react";
-import Header from "../../components/Header";
+import NavBar from "../../components/NavBar";
 import Sidebar from "../../components/Sidebar";
-import TemplateRow from "../../components/ManageTemplates/TemplateRow";
+import TableHeading from "../../components/ManageTemplates/TableHeading";
+import UserDataRow from "../../components/ManageTemplates/UserDataRow";
 
-const ManageTemplates = () => {
-  const templates=[
-    {name:"Sign Up", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"Change Password", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"Create Admin", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"Forgot Password", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"Verification Link", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"User Sign Up", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"User Forgot Password", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"User Verification Link", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-    {name:"Create Employee", createdAt: "27-03-2024", updatedAt: "10-04-2023"},
-  ]
+const ConnectionRequests = () => {
+  const data = [
+    { templateName: "Sign Up", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "Change Password", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "Create Admin", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "Forgot Password", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "Verification Link", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "User Sign Up", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "User Forgot Password", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "User Verification Link", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+    { templateName: "Create Employee", createdAt: "27-03-2024", updatedAt: "10-04-2023" },
+  ];
+
   return (
     <>
-      <Header />
-      <div className="flex justify-between px-12">
-        <Sidebar />
-        <div className="w-full">
-          <div className="p-2.5 rounded-md bg-white m-3 shadow-sm text-black sm:text-base text-xs self-stretch border">
-            <h1 className="bg-[#214139] text-white text-left py-1.5 pl-5 text-base">
-              EMAIL TEMPLATES
-            </h1>
-            <div className="m-3 flex flex-col border">
-              <div className="bg-[#ddd] rounded-tr-md rounded-tl-md font-normal border border-slate-300 flex  justify-evenly">
-                <span className="border-x-white border-x-2 w-2/5 p-3">
-                  Template Name
-                </span>
-                <span className="border-x-white border-x-2 w-1/4 p-3">
-                  Created At
-                </span>
-                <span className="border-x-white border-x-2 w-1/4 p-3">
-                  Updated At
-                </span>
-                <span className="border-x-white border-x-2 w-[10%] p-3">
-                  Action
-                </span>
-              </div>
-              {templates.map(({name, createdAt, updatedAt})=><TemplateRow templateName={name} createdAt={createdAt} updatedAt={updatedAt}/>)}
-            </div>
+    
+      <NavBar />
+      <div className="w-full flex">
+        <Sidebar  />
+        <div className="p-4 w-full flex flex-col gap-4 relative">
+          <h1 className="text-2xl">EMAIL TEMPLATES</h1>
+          <TableHeading />
+          <div className="bg-white min-h-96 max-h-[75vh] h-full overflow-y-scroll overflow-x-hidden connectionRequest-container shadow-xl">
+            {data.map(({ templateName, createdAt, updatedAt }, index) => (
+              <UserDataRow
+                key={index}
+                templateName={templateName}
+                createdAt={createdAt}
+                updatedAt={updatedAt}
+              />
+            ))}
           </div>
         </div>
       </div>
@@ -49,4 +42,4 @@ const ManageTemplates = () => {
   );
 };
 
-export default ManageTemplates;
+export default ConnectionRequests;
