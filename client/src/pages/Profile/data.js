6a64117759edabfced8cd07460ed22a688085d9a
Divya Agrawal <divyaagrawal0747@gmail.com>
2024-08-24 16:07:49 +0530

    add: Posts/Videos Tab in Profile Page

---
+++ client/src/pages/Profile/data.js
@@ -0,0 +1,142 @@
+export const postsList = [
+    {
+      img: "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+      date: "2024-08-23",
+      likes: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"},
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"},
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      comments: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"},
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"},
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"}
+      ],
+      shares: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      tags: [
+        {title: "tech"},
+        {title: "writing"},
+        {title: "tag01"}
+      ]
+    },
+    {
+      img: "https://www.datocms-assets.com/46272/1633199491-1633199490440.jpg?auto=format&fit=max&w=1200",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+      date: "2024-08-23",
+      likes: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      comments: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"}
+      ],
+      shares: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      tags: [
+        {title: "tech"},
+        {title: "writing"},
+        {title: "tag01"}
+      ]
+    },
+    {
+      img: "https://hips.hearstapps.com/hmg-prod/images/nature-quotes-landscape-1648265648.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+      date: "2024-08-23",
+      likes: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      comments: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"}
+      ],
+      shares: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      tags: [
+        {title: "tech"},
+        {title: "writing"},
+        {title: "tag01"}
+      ]
+    },
+    {
+      img: "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+      date: "2024-08-23",
+      likes: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      comments: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"}
+      ],
+      shares: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      tags: [
+        {title: "tech"},
+        {title: "writing"},
+        {title: "tag01"}
+      ]
+    },
+    {
+      img: "https://www.datocms-assets.com/46272/1633199491-1633199490440.jpg?auto=format&fit=max&w=1200",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+      date: "2024-08-23",
+      likes: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      comments: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"}
+      ],
+      shares: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      tags: [
+        {title: "tech"},
+        {title: "writing"},
+        {title: "tag01"}
+      ]
+    },
+    {
+      img: "https://hips.hearstapps.com/hmg-prod/images/nature-quotes-landscape-1648265648.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*",
+      caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+      date: "2024-08-23",
+      likes: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      comments: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1", description:"this is a test comment"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2", description:"this is a test comment"}
+      ],
+      shares: [
+        {icon:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg", title:"Person1"},
+        {icon:"https://media.istockphoto.com/id/1342013370/photo/a-happy-young-womans-headshot-in-a-yellow-sweater.jpg?s=612x612&w=0&k=20&c=WlwbNg4VeXsMJKaIsXymPhs648rO6e9MYBk0yZwqL5w=", title:"Person2"}
+      ],
+      tags: [
+        {title: "tech"},
+        {title: "writing"},
+        {title: "tag01"}
+      ]
+    }
+  ];
