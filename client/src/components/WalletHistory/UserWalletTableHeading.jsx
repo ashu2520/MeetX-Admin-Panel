7d6d01dc4e28a3f6ebf7e243f72f849d9f77a150
kartikeypant18m <kartike121@gmail.com>
2024-08-30 04:42:52 +0530

    userAuth and update navbar

--- client/src/components/WalletHistory/UserWalletTableHeading.jsx
+++ client/src/components/WalletHistory/UserWalletTableHeading.jsx
@@ -1,40 +1,64 @@
 import React from "react";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
-const UserWalletTableHeading = ({ onSort }) => {
+const UserWalletTableHeading = ({ onSort, sortColumn, sortDirection }) => {
+  const renderSortIcon = (column) => {
+    if (sortColumn !== column) return null;
+    if (sortDirection === "asc")
+      return <FaArrowUp className="inline ml-2" size={14} color="#000" />;
+    if (sortDirection === "desc")
+      return <FaArrowDown className="inline ml-2" size={14} color="#000" />;
+  };
+
   return (
     <div className="bg-white border flex shadow-md">
       <div
         className="border w-1/6 p-2 font-semibold cursor-pointer"
         onClick={() => onSort("user_id")}
       >
-        User ID
+        User ID {renderSortIcon("user_id")}
       </div>
       <div
         className="border w-1/6 p-2 font-semibold cursor-pointer"
         onClick={() => onSort("schedule_id")}
       >
-        Schedule ID
+        Schedule ID {renderSortIcon("schedule_id")}
       </div>
       <div
         className="border w-1/6 p-2 font-semibold cursor-pointer"
         onClick={() => onSort("type")}
       >
-        Type
+        Type {renderSortIcon("type")}
       </div>
-      <div className="border w-2/6 p-2 font-semibold">Account No.</div>
-      <div className="border w-1/6 p-2 font-semibold">IFSC Code</div>
-      <div className="border w-1/6 p-2 font-semibold">UPI ID</div>
+      <div
+        className="border w-2/6 p-2 font-semibold cursor-pointer"
+        onClick={() => onSort("accountNo")}
+      >
+        Account No. {renderSortIcon("accountNo")}
+      </div>
+      <div
+        className="border w-1/6 p-2 font-semibold cursor-pointer"
+        onClick={() => onSort("ifsc")}
+      >
+        IFSC Code {renderSortIcon("ifsc")}
+      </div>
+      <div
+        className="border w-1/6 p-2 font-semibold cursor-pointer"
+        onClick={() => onSort("upi")}
+      >
+        UPI ID {renderSortIcon("upi")}
+      </div>
       <div
         className="border w-1/6 p-2 font-semibold cursor-pointer"
         onClick={() => onSort("status")}
       >
-        Status
+        Status {renderSortIcon("status")}
       </div>
       <div
         className="border w-1/6 p-2 font-semibold cursor-pointer"
         onClick={() => onSort("amount")}
       >
-        Amount
+        Amount {renderSortIcon("amount")}
       </div>
     </div>
   );
